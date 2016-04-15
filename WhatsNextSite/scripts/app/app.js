ffc28996e45a07a0cf1a46ca50c2df1357b2fb69
corp\ib250092 <irhadba@maestralsolutions.com>
2016-04-15 18:45:40 +0200

    Website added and angularjs installed and initially configured

---
+++ WhatsNextSite/scripts/app/app.js
@@ -0,0 +1,25 @@
+﻿var whatsNextApp = angular.module("whatsNextApp", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
+
+//whatsNextApp.config(function (toastrConfig) {
+//});
+
+
+//whatsNextApp.constant("Config", {
+//    source: "http://localhost:62341/api/"
+//});
+
+//whatsNextApp.run(function (stateService, $location) {
+//    if (!stateService.data.currentUser) {
+//        $location.path("/login");
+//    }
+//});
+
+whatsNextApp.config(function ($routeProvider) {
+
+    $routeProvider
+        .when("/content", {
+            templateUrl: "views/content.html",
+            controller: "contentController"
+        })
+        .otherwise({ redirectTo: "/content" });
+});
---
+++ WhatsNextSite/scripts/app/controllers/contentController.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    angular.module('whatsNextApp').controller("contentController", function ($scope) {
+
+    });
+}());
